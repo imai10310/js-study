2d358b7db3076fb16f7800668cce59645fdabbe5
いまい <17039vbu@gmail.com>
2022-09-01 22:39:40 +0900

    課題①開始（for,if,乱数）

---
+++ jsTask/jsTask1.js
@@ -0,0 +1,28 @@
+// 内容
+// 1~100内で生成された乱数を使用して
+// for文を1000回 回してください
+
+// 同じ数字が連続で出た場合
+// ・for文を終了し、「同じ数がでた◯と◯！！！」　と表示してください
+
+// 同じ数字が連続で出なかった場合
+// ・「ざまあ」と表示してください
+
+// 補足
+// ・if文使用
+// ・for文使用
+// ・変数使用
+// ・Math Rondom使用
+
+let num = 0;
+for (let i = 0; i <= 100; math.round()) {}
+
+// let num = 4;
+// console.log('ループ開始');
+// for (
+//   let rand = Math.round(Math.random() * 10);
+//   rand !== num;
+//   rand = Math.round(Math.random() * 10)
+// ) {
+//   console.log('randの値' + rand);
+// }
