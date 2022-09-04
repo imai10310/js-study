bd4b362fe93c4a3fa29989a15e70bd8096ed534c
いまい <17039vbu@gmail.com>
2022-09-04 16:30:57 +0900

    練習課題３問目レビュー待ち

--- jsBookStudy/jsLearn.js
+++ jsBookStudy/jsLearn.js
@@ -1,3 +1,11 @@
 'use strict';
-console.log('hello');
-console.log('hello');
+// ●ループの入れ子
+console.log('外のループ開始');
+for (let i = 0; i < 9; i++) {
+  console.log('内のループ開始');
+  for (let j = 0; j < 8; j++) {
+    console.log(`i:j=${i}:${j}`);
+  }
+  console.log('内のループ終了');
+}
+console.log('外のループ終了');
---
+++ jsTask/jsTask(practice).js
@@ -0,0 +1,86 @@
+//-----------------------------------------------------
+//forで１００回回す
+//求数の時は「偶数」
+//奇数の時は「奇数」 と表示する
+
+//●●自分の答え●●
+let resultText = '';
+for (let i = 0; i < 100; i++) {
+  const randomNum = i; //
+  if (randomNum % 2 === 0) {
+    resultText = `偶数です${i}`;
+    // break;
+  }
+  if (randomNum % 2 !== 0) {
+    resultText = `奇数です${i}`;
+  }
+}
+console.log(resultText);
+
+//●●正しい解答
+// for (let i = 0; i < 100; i++) {
+//   console.log(i);
+//   if (i % 2 === 0) {
+//     console.log('偶数');
+//   } else {
+//     console.log('奇数');
+//   }
+// }
+
+//-----------------------------------------
+// forループ50回回す
+//出てきた数字を全部足す
+
+// let saveNum = 0;
+// for (let i = 0; i < 50; i++) {
+//   saveNum += i;
+// }
+// console.log(saveNum);
+//------------------------------------------------
+//鬼簡単
+//任意の回数回す
+//始めるよーーーーーー
+// ***
+// おわり！！
+// console.log('はじめるよ');
+// let str = '';
+// for (let i = 0; i < 5; i++) {
+//   str += '★';
+// }
+// console.log(str);
+// console.log('おわり');
+
+//------------------------------------------------
+
+//for文10回回す
+//1が出た場合　「１です」
+//7だったら「７です」
+//それ以外「うんち」
+//備考（if,switchどちらも作る）
+
+//●●for●●
+// for (let i = 0; i < 10; i++) {
+//   if (i === 1) {
+//     console.log('１です');
+//   } else if (i === 7) {
+//     console.log('７です');
+//   } else {
+//     console.log('うんち');
+//   }
+// }
+
+//●●switch●●
+// for (let i = 0; i < 10; i++) {
+//   switch (i) {
+//     case 1:
+//       console.log('1です');
+//       break;
+//     case 7:
+//       console.log('7です');
+//       break;
+//     default:
+//       console.log('うんち');
+//       break;
+//   }
+// }
+//--------------------------------------------------------
