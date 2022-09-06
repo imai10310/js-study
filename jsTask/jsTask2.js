94484c10bf3e65f40bf1c34bad3460d76ceaf363
いまい <17039vbu@gmail.com>
2022-09-06 23:55:55 +0900

    解説いれた（途中）

--- jsTask/jsTask2.js
+++ jsTask/jsTask2.js
@@ -2,11 +2,12 @@
 // ツリーの高さを指定できるようにしてください
 
 let str = '';
-const row = 9;
+const row = 9; // row:行 行の変数を作ることで、行を指定できる。
 // 縦行
 for (let i = 0; i < row; i++) {
   // 横列
   for (let j = 0; j < row - i; j++) {
+    // row-1:下にいくたび、文字列減ってく
     str += ' ';
   }
   for (let j = 0; j < i * 2 + 1; j++) {
