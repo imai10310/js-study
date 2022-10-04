5e78ab0ebff8566ee8e4ad10ce3103a1aefd2b19
imai10310 <17039vbu@gmail.com>
2022-10-04 18:41:23 +0900

    自動販売機を継承させてアサヒ自動販売機クラスを作成

--- machine/asahi.mjs
+++ machine/asahi.mjs
@@ -1,9 +1,7 @@
 import VendingMachine from './vendingMachine.mjs';
 
 export default class Asahi extends VendingMachine {
-  constructor() {
+  constructor(drinkName, price) {
     super(drinkName, price);
-  export default}
+  }
 }
-
-//　アサヒはお酒も売っていて、２０歳以上でないと買えない。
--- machine/index.mjs
+++ machine/index.mjs
@@ -1,12 +1,13 @@
 import VendingMachine from './vendingMachine.mjs';
+import Asahi from './Asahi.mjs';
 
-const cola = new VendingMachine('コカ・コーラ', 150);
-cola.checkPrice(150);
-cola.sellDrink(1000);
+// const cola = new VendingMachine('コカ・コーラ', 150);
+// cola.checkPrice(150);
+// cola.sellDrink(1000);
 
-const monsterEnergy = new VendingMachine('モンスターエナジー', 210);
-monsterEnergy.checkPrice(150);
-monsterEnergy.sellDrink(1000);
+// const monsterEnergy = new VendingMachine('モンスターエナジー', 210);
+// monsterEnergy.checkPrice(150);
+// monsterEnergy.sellDrink(1000);
 
 const asahiBeer = new Asahi();
 asahiBeer.sellDrink(1000);
